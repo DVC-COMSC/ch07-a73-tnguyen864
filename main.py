8499566049258e68e25842d6728381956e7e48db
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 08:19:51 -0700

    Assignment 7-3

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-
-
+# List of numbers
 numbers = [5, 20, 30, 35, 50]
 
+# Get insertion value and add to list
 insval = int(input('Enter the insertion value: '))
-# ******************************
-# Make your Code
-# ******************************
+numbers.append(insval)
 
-prinnt (numbers)
+# Sort list
+numSorted = []
+for i in range(len(numbers)):
+    minIndex = numbers.index(min(numbers))
+    transfer = numbers.pop(minIndex)
+    numSorted.append(transfer)
+
+# Print sorted numbers
+print (numSorted)
